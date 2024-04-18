67863baa1ddf2e7bc444e31e08515f2e2c57ad45
victorportodev <victorportodev@gmail.com>
2024-04-18 20:08:10 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosH();
+        } else {
+            exibeElementosH();
+        }
+    });
+
+
+    // Seção de atrações, programção das abas.
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function (event) {
+            const abaAlvo = event.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            event.target.classList.add('shows__tabs__button--is-active');
+
+        });
+    }
+    // Seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', function(event) {
+            abreOuFechaResposta(event);
+        });
+    }
+});
+
+function abreOuFechaResposta(event) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = event.target.parentNode;
+    elementoPai.classList.toggle(classe);
+}
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
+function escondeTodasAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
+
+function ocultaElementosH() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosH() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
